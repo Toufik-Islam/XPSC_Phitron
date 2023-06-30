ab2c2ca7d94bb80985d7363bd104782104db4f9b
Toufik-Islam <toufikislam268@gmail.com>
2023-06-30 21:57:39 +0600

    Week 4 Day 6

---
+++ Week 4/Day 6/D_Same_Differences.cpp
@@ -0,0 +1,46 @@
+/*
+\~~~~~~~~~~~~~~~~~\
+ \    TOUFIK       \
+  \        ISLAM    \
+   \~~~~~~~~~~~~~~~~~\
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define nl '\n'
+#define md 1000000007
+#define Faster                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+
+void solve()
+{
+    int n;
+    cin >> n;
+    unordered_map<int, int> mp;
+    ll ans = 0;
+    for (int i = 0, x; i < n; i++)
+    {
+        cin >> x;
+        ans += mp[(x - i)];
+        mp[x - i]++;
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    Faster;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
