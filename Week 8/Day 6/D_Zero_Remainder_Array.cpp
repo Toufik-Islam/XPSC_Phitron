f551afb04cae93ac0d602a0eb70fd341dd4313d8
Toufik-Islam <toufikislam268@gmail.com>
2023-08-01 15:50:59 +0600

    Week 8 Day 6

---
+++ Week 8/Day 6/D_Zero_Remainder_Array.cpp
@@ -0,0 +1,75 @@
+/*\~~~~~~~~~~~~~~~~~\
+   \    TOUFIK       \
+    \        ISLAM    \
+     \~~~~~~~~~~~~~~~~~\*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = (int)1e6 + 5;
+const int M = (int)1e9 + 5;
+const int md = (int)1e9 + 7;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define mii map<int, int>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define ff first
+#define ss second
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define pb push_back
+#define nl '\n'
+#define sp << ' ' <<
+#define print_ara(x, st, ed)      \
+    for (int i = st; i < ed; i++) \
+        cout << x[i] << " \n"[i == ed - 1];
+#define print_map(t) \
+    for (auto u : t) \
+        cout << u.ff sp u.ss << nl;
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+void TC()
+{
+    ll n, k;
+    cin >> n >> k;
+    mii mp;
+    int mx_occ = 0;
+    for (int i = 0, x; i < n; i++)
+    {
+        cin >> x;
+        if (x % k)
+            mx_occ = max(mx_occ, ++mp[k - (x % k)]);
+    }
+    if(!mx_occ)
+    {
+        cout << "0\n";
+        return;
+    }
+    ll val = 0;
+    for (auto &i : mp)
+        if (i.ss == mx_occ)
+            val = i.ff;
+
+    cout << ((mx_occ - 1) * k + val + 1) << nl;
+}
+
+int main()
+{
+    FastIO;
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case " << i << ": ";
+        TC();
+    }
+    return 0;
+}
