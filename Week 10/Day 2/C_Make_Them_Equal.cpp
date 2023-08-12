bb8b1f0db8693b55aa7528625d2fb7971c502524
Toufik-Islam <toufikislam268@gmail.com>
2023-08-12 11:45:49 +0600

    Week 10 Day 2

---
+++ Week 10/Day 2/C_Make_Them_Equal.cpp
@@ -0,0 +1,74 @@
+/*\~~~~~~~~~~~~~~~~~\
+   \    TOUFIK       \
+    \        ISLAM    \
+     \~~~~~~~~~~~~~~~~~\*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = (int)1e6 + 5;
+const int M = (int)1e9 + 5;
+const int md = (int)1e9 + 7;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define mii map<int, int>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define ff first
+#define ss second
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define pb push_back
+#define nl '\n'
+#define sp << ' ' <<
+#define print_ara(x, st, ed)      \
+    for (int i = st; i < ed; i++) \
+        cout << x[i] << " \n"[i == ed - 1];
+#define print_map(t) \
+    for (auto u : t) \
+        cout << u.ff sp u.ss << nl;
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+void TC()
+{
+    int n;
+    string A;
+    char c;
+    cin >> n >> c >> A;
+    if (count(all(A), c) == n)
+        cout << "0\n";
+    else
+    {
+        for (int i = n / 2; i < n; i++)
+        {
+            if (A[i] == c)
+            {
+                cout << "1\n"
+                     << i + 1 << nl;
+                return;
+            }
+        }
+        cout << "2\n"
+             << n - 1 sp n << nl;
+    }
+}
+
+int main()
+{
+    FastIO;
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case " << i << ": ";
+        TC();
+    }
+    return 0;
+}
