e3262472ef8e3f33154158c694f9faac7e81ba7a
Toufik-Islam <toufikislam268@gmail.com>
2023-08-13 18:46:53 +0600

    Week 10 Day 3

---
+++ Week 10/Day 3/B_Diameter_of_Graph.cpp
@@ -0,0 +1,69 @@
+/*\~~~~~~~~~~~~~~~~~\
+   \    TOUFIK       \
+    \        ISLAM    \
+     \~~~~~~~~~~~~~~~~~\*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = (int)1e6 + 5;
+const int M = (int)1e9 + 5;
+const int md = (int)1e9 + 7;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define mii map<int, int>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define ff first
+#define ss second
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define pb push_back
+#define nl '\n'
+#define sp << ' ' <<
+#define print_ara(x, st, ed)      \
+    for (int i = st; i < ed; i++) \
+        cout << x[i] << " \n"[i == ed - 1];
+#define print_map(t) \
+    for (auto u : t) \
+        cout << u.ff sp u.ss << nl;
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+void TC()
+{
+    ll n, m, k;
+    cin >> n >> m >> k;
+    if (n - 1 <= m and m <= (n * (n - 1)) / 2)
+    {
+        if (n == 1 && 0 < k - 1)
+            cout << "YES\n";
+        else if (m == (n * (n - 1)) / 2 and 1 < k - 1)
+            cout << "YES\n";
+        else if (2 < k - 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    FastIO;
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case " << i << ": ";
+        TC();
+    }
+    return 0;
+}
