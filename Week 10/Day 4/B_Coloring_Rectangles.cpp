9ac7919f2d9222c412d631d344f58479fa81dabc
Toufik-Islam <toufikislam268@gmail.com>
2023-08-14 19:42:20 +0600

    Week 10 Day 4

---
+++ Week 10/Day 4/B_Coloring_Rectangles.cpp
@@ -0,0 +1,33 @@
+/*\~~~~~~~~~~~~~~~~~\
+   \    TOUFIK       \
+    \        ISLAM    \
+     \~~~~~~~~~~~~~~~~~\*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+
+void TC()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << (a * b / 3 + ((a * b) % 3 != 0)) << nl;
+}
+
+int main()
+{
+    FastIO;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        TC();
+
+    return 0;
+}
