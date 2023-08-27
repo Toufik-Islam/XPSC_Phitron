717b21d4b8053e14f02818e4a321a97c25e24e87
Toufik-Islam <toufikislam268@gmail.com>
2023-08-27 23:51:35 +0600

    Week 12 Day 3

---
+++ Week 12/Day 3/D_Same_Differences.cpp
@@ -0,0 +1,41 @@
+/*\~~~~~~~~~~~~~~~~~\
+   \    TOUFIK       \
+    \        ISLAM    \
+     \~~~~~~~~~~~~~~~~~\*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl '\n'
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+
+void TC()
+{
+    int n;
+    cin >> n;
+    unordered_map<int, int> mp;
+    ll ans = 0;
+    for (int i = 0, x; i < n; i++)
+    {
+        cin >> x;
+        ans += mp[(x - i)];
+        mp[x - i]++;
+    }
+    cout << ans << nl;
+}
+
+int main()
+{
+    FastIO;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        TC();
+
+    return 0;
+}
