33af7691ec8a4bd1b554bdc97eca20d5022d09d6
Toufik-Islam <toufikislam268@gmail.com>
2023-09-17 23:43:04 +0600

    Week 15 Day 3

---
+++ Week 15/Day 3/C_Salyg_1_n_and_the_MEX_Game.cpp
@@ -0,0 +1,86 @@
+/*\~~~~~~~~~~~~~~~~~\
+   \    TOUFIK       \
+    \        ISLAM    \
+     \~~~~~~~~~~~~~~~~~\*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = (int)1e6 + 5;
+const int M = (int)1e9 + 5;
+const int md = (int)1e9 + 7;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define mii map<int, int>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define ff first
+#define ss second
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define pb push_back
+#define nl '\n'
+#define sp << ' ' <<
+#define print_ara(x, st, ed)      \
+    for (int i = st; i < ed; i++) \
+        cout << x[i] << " \n"[i == ed - 1];
+#define print_map(t) \
+    for (auto u : t) \
+        cout << u.ff sp u.ss << nl;
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+void TC()
+{
+    int n;
+    cin >> n;
+    set<int> A;
+    A.insert(1e9 + 5);
+    for (int i = 0, x; i < n; i++)
+    {
+        cin >> x;
+        A.insert(x);
+    }
+    int mex, cur = 0;
+    for (auto i : A)
+    {
+        // cout << i sp cur << nl;
+        if (i != cur)
+        {
+            mex = cur;
+            break;
+        }
+        cur++;
+    }
+    // cout << mex << nl << nl;
+    int q = 2 * n + 1;
+    while (q--)
+    {
+        int x;
+        cout << mex << endl;
+        cin >> x;
+        if (x == -1)
+            return;
+        // A.erase(A.find(x));
+        mex = x;
+    }
+}
+
+int main()
+{
+    FastIO;
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case " << i << ": ";
+        TC();
+    }
+    return 0;
+}
---
+++ Week 15/Day 3/Sliding_Median.cpp
@@ -0,0 +1,83 @@
+/*\~~~~~~~~~~~~~~~~~\
+   \    TOUFIK       \
+    \        ISLAM    \
+     \~~~~~~~~~~~~~~~~~\*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef __int128 lll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define all(t) t.begin(), t.end()
+#define rall(t) t.rbegin(), t.rend()
+#define forr(x, n) for (int i = x; i < n; ++i)
+#define ff first
+#define ss second
+#define pb push_back
+#define nl '\n'
+#define sp << ' ' <<
+#define print_ara(x, st, ed)      \
+    for (int i = st; i < ed; i++) \
+        cout << x[i] << " \n"[i == ed - 1];
+#define print_map(t) \
+    for (auto u : t) \
+        cout << u.ff << " -> " << u.ss << nl;
+#define FastIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr);
+
+const int N = (int)1e6 + 5;
+const ll inf = (ll)1e18 + 5;
+const int md = (int)1e9 + 7;
+
+void TC()
+{
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    ordered_set<pii> os;
+    mii mp;
+    forr(0, n) cin >> a[i];
+
+    for (int i = 0; i < k - 1; ++i)
+        os.insert({a[i], ++mp[a[i]]});
+
+    vi ans;
+    for (int i = k - 1; i < n; ++i)
+    {
+        os.insert({a[i], ++mp[a[i]]});
+        int med = os.find_by_order((k - 1) / 2)->ff;
+        ans.pb(med);
+        os.erase(os.find({a[i - k + 1], mp[a[i - k + 1]]--}));
+    }
+    print_ara(ans, 0, n - k + 1);
+}
+
+int main()
+{
+    FastIO;
+
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+        // cout << "Case " << i << ": ";
+        TC();
+    }
+    return 0;
+}
